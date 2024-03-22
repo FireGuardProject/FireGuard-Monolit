7402c7748fded2aca4485655d706366c40c06e39
Håkon <69848281+h587916@users.noreply.github.com>
2024-03-22 14:59:18 +0100

    bus_logic

--- src/frcm/logic/bus_logic.py
+++ src/frcm/logic/bus_logic.py
@@ -21,7 +21,7 @@ class FireRiskAPI:
         time_now = datetime.now()
         start_time = time_now - delta
         observations = self.client.fetch_observations(location, start=start_time, end=time_now)
-        forecast = self.client.fetch_forecast(location)
+        forecast = self.client.fetch_forecast(location, start_time, time_now)
         wd = WeatherData(created=time_now, observations=observations, forecast=forecast)
         prediction = self.compute(wd)
         return prediction 
@@ -30,7 +30,7 @@ class FireRiskAPI:
         time_now = datetime.now()
         end_time = time_now + delta
         observations = self.client.fetch_observations(location, start=time_now, end=end_time)
-        forecast = self.client.fetch_forecast(location)
+        forecast = self.client.fetch_forecast(location, time_now, end_time)
         wd = WeatherData(created=time_now, observations=observations, forecast=forecast)
         prediction = self.compute(wd)
         return prediction
@@ -38,7 +38,7 @@ class FireRiskAPI:
     def compute_specific_period(self, location: Location, start: datetime, end: datetime) -> FireRiskPrediction:
         time_now = datetime.now()
         observations = self.client.fetch_observations(location, start=start, end=end)
-        forecast = self.client.fetch_forecast(location)
+        forecast = self.client.fetch_forecast(location, start, end)
         wd = WeatherData(created=time_now, observations=observations, forecast=forecast)
         prediction = self.compute(wd)
         return prediction 
@@ -47,7 +47,7 @@ class FireRiskAPI:
         time_now = datetime.now()
         end = start + delta
         observations = self.client.fetch_observations(location, start=start, end=end)
-        forecast = self.client.fetch_forecast(location)
+        forecast = self.client.fetch_forecast(location, start, end)
         wd = WeatherData(created=time_now, observations=observations, forecast=forecast)
         prediction = self.compute(wd)
         return prediction
@@ -56,7 +56,7 @@ class FireRiskAPI:
         time_now = datetime.now()
         start = end - delta
         observations = self.client.fetch_observations(location, start=start, end=end)
-        forecast = self.client.fetch_forecast(location)
+        forecast = self.client.fetch_forecast(location, start, end)
         wd = WeatherData(created=time_now, observations=observations, forecast=forecast)
         prediction = self.compute(wd)
         return prediction
